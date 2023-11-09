da306ba094f22739d61ad2af42e9941f0ed2f8f6
Alex Pokras <a.a.pokras@gmail.com>
2023-11-09 05:14:15 -0500

    Fix the analytics dashboard to update values correctly

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -11,61 +11,17 @@ import * as fileSaver from 'file-saver';
 })
 export class AnalyticsComponent implements OnInit {
 
-  public get averageValue(): number | undefined {
-    if (!this.faceDetection.detections?.length) { return undefined; }
-
-    const average = this.faceDetection.detections
-              ?.map(value => value.value)
-              .reduce((acc, current) => acc + current, 0) / this.faceDetection.detections?.length
-
-    return Math.round(average * 100) / 100;
-  }
-
-  public get avgGroupFlow(): number | undefined {
-    if (!this.dashboard.groupFlow?.length) { return undefined; }
-
-    const average = this.dashboard.groupFlow
-              ?.map(value => value.value)
-              .reduce((acc, current) => acc + current, 0) / this.dashboard.groupFlow?.length
-
-    return Math.round(average * 100) / 100;
-  }
-
-  public get avgPeak(): number | undefined {
-    if (!this.dashboard.peak?.length) { return undefined; }
-
-    const average = this.dashboard.peak
-              ?.map(value => value.value)
-              .reduce((acc, current) => acc + current, 0) / this.dashboard.peak?.length
-
-    return Math.round(average * 100) / 100;
-  }
-
-  public get avgHappy(): number | undefined {
-    if (!this.dashboard.happy?.length) { return undefined; }
-
-    const average = this.dashboard.happy
-              ?.map(value => value.value)
-              .reduce((acc, current) => acc + current, 0) / this.dashboard.peak?.length
-
-    return Math.round(average * 100) / 100;
-  }
-
   constructor(public faceDetection: FaceDetectionService,
               public dashboard: DashboardService,
               private router: Router) { }
 
   ngOnInit(): void {
     if (!this.faceDetection.detections?.length) {
-      this.navigateToHome();
+      this.navigateToPresenterView();
     }
   }
 
-  private navigateToHome(): void {
-    this.router.navigateByUrl('');
-  }
-
-  public get getDat(): number | undefined {
+  public get downloadJson(): number | undefined {
     if (!this.faceDetection.detections?.length) { return undefined; }
 
     const fileToSave = new Blob([JSON.stringify(this.faceDetection.detections)], {
@@ -74,6 +30,46 @@ export class AnalyticsComponent implements OnInit {
 
     const time = new Date();
     fileSaver.saveAs(fileToSave, `moody_${time.toLocaleDateString().split('/').join('-')}_${time.getHours()}-${time.getMinutes()}.json`);
+  }
+
+  public get moodyScore(): string | undefined {
+    const average = this.calculateAverage(this.faceDetection.detections);
+    return average == undefined ? undefined : average.toString();
+  }
+
+  public get avgGroupFlow(): string | undefined {
+    return this.calculateAveragePercent(this.dashboard.groupFlow);
+  }
+
+  public get avgPeak(): string | undefined {
+    return this.calculateAveragePercent(this.dashboard.peak);
+  }
+
+  public get avgHappy(): string | undefined {
+    return this.calculateAveragePercent(this.dashboard.happy);
+  }
+
+  private calculateAverage(data: {[key: string]: any, value: number }[]): number | undefined {
+    if (!data?.length) { return undefined; }
+  
+    const validValues = data
+      ?.map(value => value.value)
+      .filter(value => value !== undefined && value !== null && !isNaN(value) && isFinite(value));
+  
+    if (!validValues.length) { return undefined; }
+  
+    const average = validValues.reduce((acc, current) => acc + current, 0) / validValues.length;
+
+    return parseFloat(average.toFixed(2));  // return 2 fixed-point digits
+  }
+
+  private calculateAveragePercent(data: {[key: string]: any, value: number }[]): string | undefined {
+    const avg = this.calculateAverage(data) * 100;
+    return avg.toString().split('.')[0] + '%';
+  }
+
+  private navigateToPresenterView(): void {
+    this.router.navigateByUrl('/presenterview');
   }
 
 }
